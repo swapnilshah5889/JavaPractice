468df5c407b643c3f074d9a5124ca6503c5bdef7
Swapnil Shah <swapnilshah5889@gmail.com>
2024-06-05 00:49:55 -0400

    Sharing object between threads

--- src/main/java/org/swapnilshah5889/MultiThreading/PrintNNumbers/Client.java
+++ src/main/java/org/swapnilshah5889/MultiThreading/PrintNNumbers/Client.java
@@ -3,7 +3,8 @@ package org.swapnilshah5889.MultiThreading.PrintNNumbers;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
+
+    public static void version1() {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter n: ");
@@ -16,5 +17,36 @@ public class Client {
         }
 
         System.out.println("Bye from main");
+    }
+
+
+    public static void version2() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+
+        // Common object between all threads
+        NumberPrinter2 printer = new NumberPrinter2(1);
+
+        // Each thread prints the current value and then increments it in the run method
+        for(int i=1; i<=n; i++) {
+            Thread t = new Thread(printer);
+            t.start();
+        }
+
+        System.out.println("Bye from main");
+
+    }
+
+    public static void main(String[] args) {
+
+        // Print all N numbers using different threads
+        // In un-synchronized manner
+        // version1();
+
+        // Sharing data between all threads
+        version2();
+
     }
 }
