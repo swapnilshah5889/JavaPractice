e0875b3e9d4d075ed0c3cf4be458958b184ff81c
Swapnil Shah <swapnilshah5889@gmail.com>
2024-06-04 23:48:32 -0400

    Initialized multi-threading package

---
+++ src/main/java/org/swapnilshah5889/MultiThreading/SimpleThreading/SimpleThreading.java
@@ -0,0 +1,12 @@
+package org.swapnilshah5889.MultiThreading.SimpleThreading;
+
+public class SimpleThreading {
+
+    public static void main(String[] args) {
+
+        // Print from the main thread
+        System.out.println("Currently in "+Thread.currentThread().getName()+" Thread");
+
+    }
+
+}
